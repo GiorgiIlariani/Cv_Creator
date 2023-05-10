06bd2228e7a79867da9c6d0a8b850a016bc9f44e
GiorgiIlariani <lmaisuradze3333@gmail.com>
2023-05-10 15:42:01 +0300

    working on some errors

--- src/components/formikHelpers/textFields/Textarea.jsx
+++ src/components/formikHelpers/textFields/Textarea.jsx
@@ -10,9 +10,16 @@ const TextareaComponent = (props) => {
   const { label, name, placeholder, minRows } = props;
   const [field, meta] = useField(name);
 
+  const hasError = meta.error && meta.touched && meta;
+
   return (
     <>
-      <label htmlFor={name} style={{ fontWeight: "600" }}>
+      <label
+        htmlFor={name}
+        style={{
+          fontWeight: "600",
+          color: hasError ? "crimson" : "",
+        }}>
         {label}
       </label>
       <Textarea
@@ -20,8 +27,10 @@ const TextareaComponent = (props) => {
         name={name}
         {...field}
         placeholder={placeholder}
-        sx={{ marginTop: "10px" }}
-        error={meta.error && meta.touched && meta}
+        sx={{
+          marginTop: "10px",
+        }}
+        error={hasError}
       />
     </>
   );
